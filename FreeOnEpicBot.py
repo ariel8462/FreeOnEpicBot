fa7666cd10281487a98e4aeab2522b45eb62080c
ariel8462 <ariel.amromin@gmail.com>
2021-01-17 06:31:37 +0200

    wont send a message on startup

--- FreeOnEpicBot.py
+++ FreeOnEpicBot.py
@@ -1,11 +1,8 @@
 import feedparser
-import requests
-import re
 import telegram
 from telegram.ext import Updater, CommandHandler
 import logging
 import threading
-
 
 token = "1447754233:AAHxpRbmZH2IbrVFAo8sShCGnQX3vpsw9fo"
 user_id = 1170714920
@@ -18,9 +15,9 @@ free_game_list = []
 
 
 def send_message(chat_id, text, token=token):
-
     bot = telegram.Bot(token=token)
     bot.sendMessage(chat_id=user_id, text=text)
+
 
 def get_links(url="https://www.indiegamebundles.com/category/free/rss"):
     d = feedparser.parse(url)
@@ -29,7 +26,10 @@ def get_links(url="https://www.indiegamebundles.com/category/free/rss"):
     threading.Timer(600.0, get_links).start()
     if free_link not in free_game_list:
         free_game_list.append(free_link)
-        send_message(chat_id=user_id, text=f"""{free_description}
+        if len(free_game_list) == 1:
+            pass
+        else:
+            send_message(chat_id=user_id, text=f"""{free_description}
 {free_link}""")
     return f""" {free_description}
 {free_link}"""
@@ -48,18 +48,11 @@ def free_game(update, context):
     bot.send_message(chat_id=user_id, text=free_right_now)
 
 
-#def notify_free_game(context: telegram.ext.CallbackContext):
-    #context.bot.send_message(chat_id=user_id, text=free_right_now)
-
-
 def main():
     updater = Updater(token=token, use_context=True)
-    #j = updater.job_queue
-    #j.run_repeating(notify_free_game, interval=86400)
     dp = updater.dispatcher
     logger.info('The bot has started')
     dp.add_handler(CommandHandler('freegame', free_game))
-    #j.stop()
     updater.start_polling()
     updater.idle()
 
