3649374b402aedd7dc9e5e8752793650a5c0de11
ariel8462 <ariel.amromin@gmail.com>
2021-02-16 02:20:21 +0200

    added job queue support and removed a bit of useless code

--- FreeOnEpicBot.py
+++ FreeOnEpicBot.py
@@ -1,8 +1,12 @@
 import feedparser
 import telegram
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, JobQueue
 import logging
-import threading
+
+help_message = """Hi there, I am a bot that shows the current free games on all the major game platforms out there.
+You will get automatically notified when new games become free to collect.
+If you would like to see the current free game please use the following command:  /freegame
+"""
 
 """
 A bot token for the bot, and a userID for the control over the bot
@@ -21,36 +25,34 @@ logger = logging.getLogger(__name__)
 free_game_list = []
 
 
-def send_message(chat_id, text, token=token):
+def send_message(text):
     bot = telegram.Bot(token=token)
     bot.sendMessage(chat_id=user_id, text=text)
 
 
-def get_links(url="https://www.indiegamebundles.com/category/free/rss"):
+def get_links(context):
     """
     Parses new games from the rss feed and formats them into a compact-looking message
 
-    Args:
-        url (str): rss feed of a specific site, in this case the feed of a "Game News" site
     Returns:
         a ready to go message that contains the current's free game link + description
     """
+    url = "https://www.indiegamebundles.com/category/free/rss"
     d = feedparser.parse(url)
     free_description = d.entries[0].title
     free_link = d.entries[0].link
-    threading.Timer(600.0, get_links).start()
     if free_link not in free_game_list:
         free_game_list.append(free_link)
         if len(free_game_list) == 1:
             pass
         else:
-            send_message(chat_id=user_id, text=f"""{free_description}
+            send_message(text=f"""{free_description}
 {free_link}""")
     return f""" {free_description}
 {free_link}"""
 
 
-free_right_now = get_links()
+free_right_now = get_links("")
 
 
 def free_game(update, context):
@@ -74,19 +76,19 @@ def help_command(update, context):
         return
     if update.effective_user.id != user_id:
         return
-    bot.send_message(chat_id=user_id, text=
-    """Hi there, I am a bot that shows you the current free games on all the major game platforms out there
-You will get automatically notified when new games become available for free
-If you want to see the current free game please use the following command: /freegame
-""")
+    bot.send_message(chat_id=user_id, text=help_message)
 
 
 def main():
     updater = Updater(token=token, use_context=True)
     dp = updater.dispatcher
+    job_queue = JobQueue()
+    job_queue.set_dispatcher(dp)
+    job_queue.run_repeating(callback=get_links, interval=600)
     logger.info('The bot has started')
     dp.add_handler(CommandHandler('freegame', free_game))
     dp.add_handler(CommandHandler("help", help_command))
+    job_queue.start()
     updater.start_polling()
     updater.idle()
 
