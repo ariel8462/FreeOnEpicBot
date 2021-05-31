159c031f3fb5bec299b958d512b1c3f882766df0
ariel8462 <ariel.amromin@gmail.com>
2021-05-31 12:33:06 +0300

    forgot to include help_message in sample_config

--- FreeOnEpicBot/sample_config.py
+++ FreeOnEpicBot/sample_config.py
@@ -1,1 +1,6 @@
+HELP_MESSAGE = """Hi there, I am a bot that shows the current free games on all the major game platforms out there.
+You will get automatically notified when new games become free to collect.
+If you would like to see the current free game please use the following command:  /freegame
+"""
+
 BOT_TOKEN = '123'
